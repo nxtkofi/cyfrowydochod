30bef998378d533a4bf0a42508132e919b5b2a3d
FullStackPete <psliwa.b@gmail.com>
2024-06-06 01:41:57 +0200

    Small adjustments to User Model.

--- server/src/main/java/pl/server/server/models/User.java
+++ server/src/main/java/pl/server/server/models/User.java
@@ -5,45 +5,29 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 @Entity
 @Table(name="users")
 public class User {
     @Id
     @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @Getter
+    @Setter
     private Long id;
+    @Getter
+    @Setter
     private String username;
+    @Getter
+    @Setter
     private String email;
+    @Getter
+    @Setter
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    @Getter
+    @Setter
+    private String refreshToken;
+    @Getter
+    @Setter
+    private String token;
 }
