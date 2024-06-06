cc94276d4b4ad0b1e27719d465c94a1bb96fc27a
Jakub Zając <jakubzajac@spoko.pl>
2024-06-06 21:24:30 +0200

    Typo in UserRepository, added findByEmail() in UserController.

--- server/src/main/java/pl/server/server/repositories/UserRepository.java
+++ server/src/main/java/pl/server/server/repositories/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 @Repository
 public interface UserRepository extends JpaRepository<User,String> {
     List<User> findByUsername(String username);
-    List<User> finfByEmail(String email);
+    List<User> findByEmail(String email);
 }
