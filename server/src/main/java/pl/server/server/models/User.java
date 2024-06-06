4dd7a493e6cb94bf8f198a0f7e7902015c373eef
Jakub Zając <jakubzajac@spoko.pl>
2024-06-06 02:16:43 +0200

    Conflict resolved

--- server/src/main/java/pl/server/server/models/User.java
+++ server/src/main/java/pl/server/server/models/User.java
@@ -5,29 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
 @Entity
 @Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class User {
     @Id
     @GeneratedValue(strategy =GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Long id;
-    @Getter
-    @Setter
     private String username;
-    @Getter
-    @Setter
     private String email;
-    @Getter
-    @Setter
     private String password;
-    @Getter
-    @Setter
     private String refreshToken;
-    @Getter
-    @Setter
     private String token;
+
 }
