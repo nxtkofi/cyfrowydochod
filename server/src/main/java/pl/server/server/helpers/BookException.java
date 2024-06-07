7dfc204959f8aed936c77e94b0f0290d10a233ab
Jakub Zając <jakubzajac@spoko.pl>
2024-06-07 09:56:50 +0200

    Changed BookException and CartController

--- server/src/main/java/pl/server/server/helpers/BookException.java
+++ server/src/main/java/pl/server/server/helpers/BookException.java
@@ -1,2 +1,7 @@
-package pl.server.server.helpers;public class BookException {
+package pl.server.server.helpers;
+
+public class BookException extends Exception {
+    public BookException(String message) {
+        super(message);
+    }
 }
