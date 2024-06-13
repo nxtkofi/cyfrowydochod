61975a2c71c738bc79f036315019fc39502a901f
Jakub Zając <jakubzajac@spoko.pl>
2024-06-13 12:24:22 +0200

    Another attempt to merge commits.

--- server/src/main/java/pl/server/server/ServerApplication.java
+++ server/src/main/java/pl/server/server/ServerApplication.java
@@ -17,13 +17,14 @@ import pl.server.server.repositories.BookRepository;
 import pl.server.server.repositories.OrdersRepository;
 import pl.server.server.repositories.UserRepository;
 import pl.server.server.services.OrdersService;
+import pl.server.server.services.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class ServerApplication implements CommandLineRunner {
+public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
@@ -46,24 +47,37 @@ public class ServerApplication implements CommandLineRunner {
 // 	public void run(String... args) throws Exception {
 
 //		userController.createUser(new User("Jakub","@gra.pl","xyz"));
+//		userController.createUser(new User("Krzysztof Krawczyk","KrzysztofKrawczyk@onet.pl","Krawczykthebest123@"));
+//		userService.registerUser(userRepository.findByEmail("KrzysztofKrawczyk@onet.pl"));
+//		userService.registerUser(userController.findByEmail("@gra.pl"));
 
-//		billingAddressController.addBillingAddress(String.valueOf(userController.findByEmail("@gra.pl").getId()),new BillingAddress("Jakub Kaczyński","ul.publiczna",8,"42-986","Warszawa"));
-		var user = userController.findByEmail("@gra.pl").getId();
+//		billingAddressController.addBillingAddress(String.valueOf(userController.findByEmail("@gra.pl").g0etId()),new BillingAddress("Jakub Kaczyński","ul.publiczna",8,"42-986","Warszawa"));
+//		billingAddressController.addBillingAddress(userController.findByEmail("KrzysztofKrawczyk@onet.pl").getId(),new BillingAddress("Krzysztof Krawczyk","ul.Słonecznikowa",3,"32-456","Kraków"));
+//		var user = userController.findByEmail("@gra.pl").getId();
 //		billingAddressController.UpdateBillingAddress(user,new BillingAddress("Jakub Kaczyński","ul.publiczna",10,"42-986","Warszawa"));
 //
 //		var cos = billingAddressController.getBillingAddress(userController.findByEmail("@gra.pl").getId());
 //		System.out.println(cos.getStreetName());
 
 //		billingAddressController.deleteBillingAddress(userController.findByEmail("@gra.pl").getBillingAddress().getAddressId());
+//		bookController.createBook(new Book("Witcher","Fantasy","Sapkowski",40));
+//		bookController.createBook(new Book("Niezgodna","IDK","ktos",35));
+//		bookController.createBook(new Book("Harry potter","Fantasy","J.K.Rowling",50));
 
 
+//		bookController.updateBook("73899dcd-5291-4c6c-8c69-ba9c845f6a4b",new Book("Sapkowkski","Witcher","Fantasy",40));
 //		List<String> allBooks = new ArrayList<>();
-//		allBooks.add(bookController.getBookById("39396fc7-7da1-4c6c-a686-7d249c7a51d1").getId());
-//		allBooks.add(bookController.getBookById("73899dcd-5291-4c6c-8c69-ba9c845f6a4b").getId());
-//		allBooks.add(bookController.getBookById("aff07518-faa0-4504-bca5-80245fb354da").getId());
-//		ordersService.createOrderFromCart(allBooks , userRepository.findByEmail("@gra.pl"));
+//		allBooks.add(bookController.getBookById("21883c7e-61af-4fa6-9caf-d321c562d47f").getId());
+//		allBooks.add(bookController.getBookById("e99f04b8-a60f-432b-99cd-debcda5d8c6c").getId());
+//		allBooks.add(bookController.getBookById("ca3a85ab-93ba-4d1c-a911-ff42ab5ad8b1").getId());
+//		ordersService.createOrderFromCart(allBooks , userRepository.findByEmail("KrzysztofKrawczyk@onet.pl"));
 
 //		bookController.createBook(new Book("Harry Potter","Fantasy","R.K.Ktośtam",20));
 // 		for tests
+
+//		userController.deleteUser("610e5576-8c7a-4916-b970-72bb2798a0d2");
+//		User user = userController.getUserById("f5c00e6f-744f-464d-8792-a21ed064b172");
+//		user.setRole("User");
+
 	}
-}
+//}
