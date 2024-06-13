1f09d11039520b91367cc82f65d64b9940631140
Jakub Zając <jakubzajac@spoko.pl>
2024-06-13 13:20:34 +0200

    Added OneToMany in User class and added ManyToOne in Tickiet class.

--- server/src/main/java/pl/server/server/models/Ticket.java
+++ server/src/main/java/pl/server/server/models/Ticket.java
@@ -1,8 +1,6 @@
 package pl.server.server.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
