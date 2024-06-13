701a12e878cfb54ff659c7290fb6f4825db2c445
Jakub Zając <jakubzajac@spoko.pl>
2024-06-13 12:24:22 +0200

    Added Message to models and optional constructor to User(conflict resolved).

--- server/src/main/java/pl/server/server/ServerApplication.java
+++ server/src/main/java/pl/server/server/ServerApplication.java
@@ -1,12 +1,29 @@
 package pl.server.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import pl.server.server.controllers.BillingAddressController;
+import pl.server.server.controllers.BookController;
+import pl.server.server.controllers.OrdersController;
+import pl.server.server.controllers.UserController;
+import pl.server.server.models.BillingAddress;
+import pl.server.server.models.Book;
+import pl.server.server.models.User;
+import pl.server.server.repositories.BillingAddressRepository;
+import pl.server.server.repositories.BookRepository;
+import pl.server.server.repositories.OrdersRepository;
+import pl.server.server.repositories.UserRepository;
+import pl.server.server.services.OrdersService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-
-@SpringBootApplication
-public class ServerApplication{
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class ServerApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
---
+++ server/src/main/java/pl/server/server/models/Message.java
@@ -0,0 +1,10 @@
+package pl.server.server.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "message")
+public class Message {
+
+}
