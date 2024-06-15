d381c4ce424cdcff10d2664324b2d38fd43915c7
Jakub Zając <jakubzajac@spoko.pl>
2024-06-15 15:37:07 +0200

    Small code cleaning.

--- server/src/main/java/pl/server/server/controllers/UserController.java
+++ server/src/main/java/pl/server/server/controllers/UserController.java
@@ -4,15 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import pl.server.server.helpers.ResourceNotFoundException;
 import pl.server.server.models.User;
 import pl.server.server.repositories.UserRepository;
