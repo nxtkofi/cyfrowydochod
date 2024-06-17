ccaab54e58ff4155b66b434ec4792375aae69856
Jakub Zając <jakubzajac@spoko.pl>
2024-06-17 13:41:30 +0200

    Added to repositories @Repository.

--- server/src/main/java/pl/server/server/repositories/OrdersRepository.java
+++ server/src/main/java/pl/server/server/repositories/OrdersRepository.java
@@ -1,11 +1,12 @@
 package pl.server.server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.server.server.models.Order;
 
 import java.time.LocalDateTime;
 import java.util.List;
-
+@Repository
 public interface OrdersRepository extends JpaRepository<Order,String> {
     List<Order> findByOrderDate(LocalDateTime orderDate);
 }
