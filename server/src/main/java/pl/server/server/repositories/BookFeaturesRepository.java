be94ade26cda7823ab7901b2ab8d519bfac3eb77
Jakub Zając <jakubzajac@spoko.pl>
2024-06-20 21:54:21 +0200

    Added @Repository.

--- server/src/main/java/pl/server/server/repositories/BookFeaturesRepository.java
+++ server/src/main/java/pl/server/server/repositories/BookFeaturesRepository.java
@@ -1,7 +1,8 @@
 package pl.server.server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.server.server.models.BookFeatures;
-
+@Repository
 public interface BookFeaturesRepository extends JpaRepository<BookFeatures,String> {
 }
--- server/src/main/java/pl/server/server/repositories/IconElementsRepository.java
+++ server/src/main/java/pl/server/server/repositories/IconElementsRepository.java
@@ -1,7 +1,8 @@
 package pl.server.server.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import pl.server.server.models.IconElements;
-
+@Repository
 public interface IconElementsRepository extends JpaRepository<IconElements,String> {
 }
