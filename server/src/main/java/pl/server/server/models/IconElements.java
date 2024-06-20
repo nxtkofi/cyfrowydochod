e763109019277438ed7ef6c13358d955a46ffad3
Jakub Zając <jakubzajac@spoko.pl>
2024-06-20 21:50:23 +0200

    Added @Column(nullable = false), @JsonIgnore, @ToString.Exclude, @ToString and constructor.

--- server/src/main/java/pl/server/server/models/IconElements.java
+++ server/src/main/java/pl/server/server/models/IconElements.java
@@ -1,9 +1,7 @@
 package pl.server.server.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -14,13 +12,24 @@ import org.hibernate.annotations.UuidGenerator;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@ToString
 public class IconElements {
     @Id
     @UuidGenerator
+    @JsonIgnore
     private String id;
+    @Column(nullable = false)
     private String iconProps;
+    @Column(nullable = false)
     private String text;
 
     @ManyToOne
+    @JsonIgnore
+    @ToString.Exclude
     private Book book;
+
+    public IconElements(String iconProps, String text) {
+        this.iconProps = iconProps;
+        this.text = text;
+    }
 }
