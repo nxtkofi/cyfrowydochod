06c0e626dc8b8512b5f7d30980c542df513fcd96
Jakub Zając <jakubzajac@spoko.pl>
2024-06-21 11:53:30 +0200

    Added method removeFeatureFromBook.

--- server/src/main/java/pl/server/server/repositories/FeatureRepository.java
+++ server/src/main/java/pl/server/server/repositories/FeatureRepository.java
@@ -3,6 +3,8 @@ package pl.server.server.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.server.server.models.Feature;
+import java.util.List;
 @Repository
 public interface FeatureRepository extends JpaRepository<Feature,String> {
+    List<Feature> findByDescription(String description);
 }
