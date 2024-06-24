3c9c2a59ccb3e2da728f39c9d230460cfa3d1d68
Jakub Zając <jakubzajac@spoko.pl>
2024-06-24 22:53:59 +0200

    Added bookService and changed bookController, added to billinaddressService try catch.BookController and BillingAddressController ready for tests.

--- server/src/main/java/pl/server/server/services/BillingAddressService.java
+++ server/src/main/java/pl/server/server/services/BillingAddressService.java
@@ -1,6 +1,7 @@
 package pl.server.server.services;
 
 import jakarta.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.server.server.helpers.ResourceNotFoundException;
@@ -20,50 +21,63 @@ public class BillingAddressService {
     UserRepository userRepository;
 
     public BillingAddress getBillingAddress(String userId){
-        return billingAddressRepository.findByUserId(userId);
+        try {
+            return billingAddressRepository.findByUserId(userId);
+        } catch (Exception ex){
+            System.err.println(ex);
+            return null;
+        }
     }
 
     @Transactional
     public BillingAddress createBillingAddress(String userId,BillingAddress newBillingAddress) {
-        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        try {
+            User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-        user.setBillingAddress(newBillingAddress);
-        newBillingAddress.setUser(user);
-        userRepository.save(user);
-        return billingAddressRepository.save(newBillingAddress);
+            user.setBillingAddress(newBillingAddress);
+            newBillingAddress.setUser(user);
+            userRepository.save(user);
+            return billingAddressRepository.save(newBillingAddress);
+        } catch (Exception ex) {
+            System.err.println(ex);
+            return null;
+        }
     }
 
-    public BillingAddress updateBillingAddress(String userId,BillingAddress newBillingAddress) throws ResourceNotFoundException {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    public BillingAddress updateBillingAddress(String userId,BillingAddress newBillingAddress) {
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
-        if(!billingAddressRepository.existsById(user.getBillingAddress().getAddressId())) {
-            throw new ResourceNotFoundException("user billing address does not exist");
+            if (!billingAddressRepository.existsById(user.getBillingAddress().getAddressId())) {
+                throw new ResourceNotFoundException("user billing address does not exist");
+            }
+
+            BillingAddress addressToUpdate = user.getBillingAddress();
+            BeanUtils.copyProperties(newBillingAddress,addressToUpdate,"addressId");
+            billingAddressRepository.save(addressToUpdate);
+            return addressToUpdate;
+        } catch (Exception ex) {
+            System.err.println(ex);
+            return null;
         }
-
-        BillingAddress addressToUpdate = user.getBillingAddress();
-
-        addressToUpdate.setCity(newBillingAddress.getCity());
-        addressToUpdate.setFullName(newBillingAddress.getFullName());
-        addressToUpdate.setZipCode(newBillingAddress.getZipCode());
-        addressToUpdate.setHouseNumber(newBillingAddress.getHouseNumber());
-        addressToUpdate.setStreetName(newBillingAddress.getStreetName());
-
-        billingAddressRepository.save(addressToUpdate);
-        return addressToUpdate;
     }
 
     @Transactional
     public void deleteBillingAddress(String billingAddressId) {
-        BillingAddress billingAddress = billingAddressRepository.findById(billingAddressId)
-                .orElseThrow(() -> new ResourceNotFoundException("Billing Address not found"));
+        try {
+            BillingAddress billingAddress = billingAddressRepository.findById(billingAddressId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Billing Address not found"));
 
-        User user = billingAddress.getUser();
-        if (user != null) {
-            user.setBillingAddress(null);
-            userRepository.save(user);
+            User user = billingAddress.getUser();
+            if (user != null) {
+                user.setBillingAddress(null);
+                userRepository.save(user);
+            }
+
+            billingAddressRepository.delete(billingAddress);
+        } catch (Exception ex) {
+            System.err.println(ex);
         }
-
-        billingAddressRepository.delete(billingAddress);
     }
 }
