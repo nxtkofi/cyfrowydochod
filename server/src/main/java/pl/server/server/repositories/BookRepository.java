28d2c1a8582ea12f38d7d8f836868976a8d0949c
Jakub Zając <jakubzajac@spoko.pl>
2024-06-25 01:52:07 +0200

    Few changes in repositories.

--- server/src/main/java/pl/server/server/repositories/BookRepository.java
+++ server/src/main/java/pl/server/server/repositories/BookRepository.java
@@ -1,12 +1,10 @@
 package pl.server.server.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import pl.server.server.models.Book;
 @Repository
 public interface BookRepository extends JpaRepository<Book,String> {
-    List<Book> findByTitle(String title);
+    Book findByTitle(String title);
 }
