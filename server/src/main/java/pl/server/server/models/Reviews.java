03c768a6174fff35333c5eaea7324d6964a76439
Jakub Zając <jakubzajac@spoko.pl>
2024-06-24 19:27:53 +0200

    Added Reviews model, BillingAddressService and changed BillingAddressController.

---
+++ server/src/main/java/pl/server/server/models/Reviews.java
@@ -0,0 +1,44 @@
+package pl.server.server.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "reviews")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+public class Reviews {
+
+    @Id
+    @UuidGenerator
+    private String id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private Long purchaseDate;
+    @Column(nullable = false)
+    private String avatarType;
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String text;
+
+    public Reviews(String username, Long purchaseDate, String avatarType, String text) {
+        this.username = username;
+        this.purchaseDate = purchaseDate;
+        this.avatarType = avatarType;
+        this.text = text;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    User user;
+}
