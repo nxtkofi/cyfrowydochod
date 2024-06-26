5467c54e4f4ddd73681890712c7fc77bc9856992
Jakub Zając <jakubzajac@spoko.pl>
2024-06-26 20:42:04 +0200

    Added ReviewRepository and few small changes to rest.

--- server/src/main/java/pl/server/server/repositories/ReviewRepository.java
+++ server/src/main/java/pl/server/server/repositories/ReviewRepository.java
@@ -1,2 +1,9 @@
-package pl.server.server.models;public class ReviewsRepository {
+package pl.server.server.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.server.server.models.Review;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, String> {
 }
