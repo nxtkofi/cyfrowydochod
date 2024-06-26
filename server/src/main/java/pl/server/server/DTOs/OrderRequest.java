f5edeeb175b6dbe693f9a43f8867eee41a66c9e8
Jakub Zając <jakubzajac@spoko.pl>
2024-06-26 20:26:26 +0200

    Added OrderRequest and ReviewRequest DTOs.

--- server/src/main/java/pl/server/server/DTOs/OrderRequest.java
+++ server/src/main/java/pl/server/server/DTOs/OrderRequest.java
@@ -1,2 +1,12 @@
-package pl.server.server.DTOs;public class OrderRequest {
+package pl.server.server.DTOs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+@Getter
+@Setter
+public class OrderRequest {
+    private List<String> booksId;
+
 }
