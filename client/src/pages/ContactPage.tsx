cc3d1265a7e526252d7abfb328676f8dacdef5cd
FullStackPete <psliwa.b@gmail.com>
2025-03-09 16:07:05 +0100

    refactor: contact form is now using shadcn components

--- client/src/pages/ContactPage.tsx
+++ client/src/pages/ContactPage.tsx
@@ -1,22 +1,9 @@
 import ContactUsForm from "@/components/ui/ContactPage/ContactUsForm";
-import TextDefault from "@/components/ui/HomePage/TextDefault";
-import Wrapper from "@/components/ui/wrapper";
 import useAuth from "@/hooks/useAuth";
 
 function ContactPage() {
   const { auth } = useAuth();
-  return (
-    <Wrapper className="md:border-2 md:border-gray-400 md:rounded-lg md:px-16 md:py-8">
-      <TextDefault className="" bigTitle center variant="default">
-        Contact us
-      </TextDefault>
-      <TextDefault center variant="secondary">
-        {" "}
-        Leave a ticket, we'll respond to you shortly.
-      </TextDefault>
-      <ContactUsForm accessDisabled={auth?.accessToken ? false : true} />
-    </Wrapper>
-  );
+  return <ContactUsForm accessDisabled={auth?.accessToken ? false : true} />;
 }
 
 export default ContactPage;
