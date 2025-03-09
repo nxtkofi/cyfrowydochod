f0fb8c14c34f63cbe9d56b041b7d9ed3cd45fc96
FullStackPete <psliwa.b@gmail.com>
2025-03-09 16:06:32 +0100

    fix(ui): add <section> tag to error page to improve ux

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -5,18 +5,20 @@ function ErrorPage() {
   const navigate = useNavigation();
 
   return (
-    <Wrapper className="h-screen">
-      <p className="text-4xl text-center font-semibold mt-24 leading-[60px]">
-        Hey, You!
-        <br /> You're lost! Go back to&nbsp;
-        <i
-          onClick={() => navigate({ path: "/" })}
-          className="md:hover:underline md:no-underline underline cursor-pointer"
-        >
-          Home page!
-        </i>
-      </p>
-    </Wrapper>
+    <section>
+      <Wrapper className="h-screen">
+        <p className="text-4xl text-center font-semibold leading-[60px]">
+          Hey, You!
+          <br /> You're lost! Go back to&nbsp;
+          <i
+            onClick={() => navigate({ path: "/" })}
+            className="md:hover:underline md:no-underline underline cursor-pointer"
+          >
+            Home page!
+          </i>
+        </p>
+      </Wrapper>
+    </section>
   );
 }
 
