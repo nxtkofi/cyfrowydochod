c45aa082d99b6544985a6c7dc7a61a59d73036ca
FullStackPete <psliwa.b@gmail.com>
2025-03-19 18:06:49 +0100

    chore: remove unused import

--- client/src/components/ui/AccessPage/LoginForm.tsx
+++ client/src/components/ui/AccessPage/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { jwtDecode } from "jwt-decode";
-import { FunctionComponent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "../input";
 import { Button } from "../button";
 import {
